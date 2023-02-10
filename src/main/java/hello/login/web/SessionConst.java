a4aafdb1a63c14c56e445dc4885a2bd9bebb6515
core <jinjs6@gmail.com>
2023-02-10 21:53:26 +0900

    refactor: 파일 위치 리팩토링

--- src/main/java/hello/login/web/SessionConst.java
+++ src/main/java/hello/login/web/SessionConst.java
@@ -1,4 +1,4 @@
-package hello.login.web.session;
+package hello.login.web;
 
 public interface SessionConst {
     String LOGIN_MEMBER = "loginMember";// interface 기본은 public static final
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,7 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
-import hello.login.web.session.SessionConst;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
